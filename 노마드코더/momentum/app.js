e50977303ab769abc873b7ff35a8f0b8ce0943c7
beautiflow <1114ny@naver.com>
2023-09-27 16:13:05 +0900

    #4.6 Loading Username

--- 노마드코더/momentum/app.js
+++ 노마드코더/momentum/app.js
@@ -3,16 +3,32 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
+// 최초 이름 입력 및 로그인
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("username", username);
-    greeting.innerHTML = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-    
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+// 환영메시지 출력
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+// 저장된 유저네임이 없는 경우
+if(savedUsername === null){
+    // 로그인 폼 출력
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else{
+    // 로그인이 되어있는 경우
+    paintGreetings(savedUsername)
+}
