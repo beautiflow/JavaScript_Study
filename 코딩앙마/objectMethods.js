4087d0d6cab2e29a9bf7146da18be8b8939dade1
beautiflow <1114ny@naver.com>
2023-10-04 16:47:28 +0900

    objectMethods.js

---
+++ 코딩앙마/objectMethods.js
@@ -0,0 +1,33 @@
+const user = {
+    name: "Mike",
+    age : 30,
+};
+
+const user2 = Object.assign({}, user);
+user2.name = "Tom";
+
+console.log(user);
+console.log(user2);
+
+
+const result = Object.keys(user);
+console.log(result);
+
+const result2 = Object.values(user);
+console.log(result2);
+
+const result3 = Object.entries(user);
+console.log(result3);
+
+let arr = [
+    ["mon", "월"],
+    ["tue", "화"],
+    ["wed", "수"],
+    ["thu", "목"],
+    ["fri", "금"],
+    ["sat", "토"],
+    ["sun", "일"],
+];
+
+const day = Object.fromEntries(arr);
+console.log(day);
